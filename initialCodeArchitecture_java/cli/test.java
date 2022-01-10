02453b92e306c46eb656121f65a9b5868cd4e862
Joseph-StJ <jps2697@mavs.uta.edu>
2022-01-10 14:18:53 -0600

    started to make the cli for testing the basic functions

---
+++ initialCodeArchitecture_java/cli/test.java
@@ -0,0 +1,25 @@
+public class test{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		//print "landing menu"
+
+		//new game sign up
+
+		//add new users
+
+		//print list of all users and info
+
+		//**eliminate half of all the users
+			//develope a function that scrolls through the list and eliminates half by a pre-determined pattern -- Game class method
+				//patterns in a enumerated class??or use an abstract pattern class
+
+		//current game status
+
+		//"You Win"			v
+
+		//"You Died"		//Maybe make these functions part of a sepereate class/Enum? {ex: endOfGameMessage}
+
+		//"You lived"		^
+	}
+}
